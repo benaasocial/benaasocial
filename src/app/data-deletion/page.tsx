11607a8a252b41aa5d0aec7c01f7f871a09fbb88
benaasocial <benaa.social0@gmail.com>
2026-05-13 03:03:06 +0400

    Add legal routes for privacy, terms, and data deletion

---
+++ src/app/data-deletion/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function DataDeletion() {
+    return (
+        <div>
+            <h1>User Data Deletion</h1>
+
+            <p>
+                Users may request deletion of their data by contacting:
+                benaa.social0@gmail.com
+            </p>
+
+            <p>
+                Users may also remove app permissions directly from their Facebook, TikTok, or Google account settings.
+            </p>
+        </div>
+    )
+}
